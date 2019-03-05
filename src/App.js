725ba691d2781ebe0e3bdebd63c18f12aa440160
Shahyn <shahynkamali@gmail.com>
2019-03-05 18:20:24 -0500

    created values  jaaaj

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Header from './components/Header.js';
-import Nav from './components/Nav.js';
-import firebase from './components/firebase.js';
-import DisplayDailyJoke from './components/DisplayDailyJoke.js';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Header from './components/Header.js'
+import Nav from './components/Nav.js'
+import firebase from './components/firebase.js'
+import DisplayDailyJoke from './components/DisplayDailyJoke.js'
 import GetJokeButton from './components/GetJokeButton'
-
-
-
-
-
-
-class VoteOnJokes extends Component {
-  componentDidMount(){
-    const dbRef = firebase.database().ref();
-    dbRef.on('value', response=>{
-    const data = response.val();
-    const newList = []
-    console.log(data) 
-  
-
-    for(let item in data){
-      console.log(item)
-      newList.push({
-        id: item.id,
-        joke: item.joke,
-        value: 0
-
-      })
-      this.setState({
-        jokesFirebaseUse: newList,
-      })
-    } 
-  
-    
-
-    })
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    )
-  }
-}
-
-
-
-
 
 
 
@@ -59,44 +15,65 @@ class App extends Component {
     this.state = {
       jokesList: null,
       jokeButtonShow: true,
-      jokesFirebase:[],
-      jokesFirebaseUse:[]
+      jokesFirebase: [],
+      jokesFirebaseUse: []
     }
   }
   /* inital function to populate firebase */
-  pushToFirebase = () =>{
+  pushToFirebase = () => {
     const dbRef = firebase.database().ref()
-    dbRef.push(this.state.jokesFirebase);
+    dbRef.push(this.state.jokesFirebase)
     this.setState({
-      jokesFirebase:[],
+      jokesFirebase: []
     })
   }
-/* inital function to populate firebase */
-/* Delete before submitting */
-componentDidMount(){
-  axios({
-    url: 'https://icanhazdadjoke.com/search',
-    method: 'GET',
-    headers: {
-      Accept: 'application/json'
-    },
-    params: {
-      limit: 30,
-      page: 1
-    }
-  }).then(results => {
-    console.log(results.data.results);
-    this.setState({
-      jokesFirebase: results.data.results
+  /* inital function to populate firebase */
+  /* Delete before submitting */
+  componentDidMount () {
+    axios({
+      url: 'https://icanhazdadjoke.com/search',
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      },
+      params: {
+        limit: 30,
+        page: 1
+      }
+    }).then(results => {
+      console.log(results.data.results)
+      this.setState({
+        jokesFirebase: results.data.results
+      })
     })
-  })
-}
+
+// setting state with our firebase jokes
+    const dbRef = firebase.database().ref()
+    dbRef.on('value', response => {
+      let data = response.val()
+      const newList = []
+      console.log(newList);
+      
+      /* console.log(data) */
+
+      for (let key in data) {
+        const  newItem = data[key]
+        newItem.forEach(joke =>{
+          newList.push({
+            id: joke.id,
+            joke: joke.joke,
+            value: 0
+          }) 
+          this.setState({
+            jokesFirebaseUse: newList
+          })
+        })
+      }
+    })
+  }
 
 
-
-
-
-
+  
   handleDailyJoke = () => {
     axios({
       url: 'https://icanhazdadjoke.com/',
@@ -106,14 +83,13 @@ componentDidMount(){
       }
     }).then(results => {
       const returnedJoke = results.data.joke
-     /*  console.log(returnedJoke) */
+      /*  console.log(returnedJoke) */
       this.setState({
         jokesList: returnedJoke,
-        jokeButtonShow: false,
+        jokeButtonShow: false
       })
     })
   }
-
 
   render () {
     return (
@@ -126,10 +102,16 @@ componentDidMount(){
           </nav>
         </header>
         {/* Daily Joke Page */}
-        {(this.state.jokeButtonShow === true) && <GetJokeButton handleDailyJoke={this.handleDailyJoke} />}
-        {(this.state.jokeButtonShow === false) && <DisplayDailyJoke dailyJoke={this.state.jokesList} handleDailyJoke={this.handleDailyJoke} />}
+        {this.state.jokeButtonShow === true && (
+          <GetJokeButton handleDailyJoke={this.handleDailyJoke} />
+        )}
+        {this.state.jokeButtonShow === false && (
+          <DisplayDailyJoke
+            dailyJoke={this.state.jokesList}
+            handleDailyJoke={this.handleDailyJoke}
+          />
+        )}
         {/* display list of jokes */}
-        <VoteOnJokes/>
         
       </div>
     )
