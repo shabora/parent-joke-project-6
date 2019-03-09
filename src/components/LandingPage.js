4939cb76cdb53bebb19ead30e114273d3f5d92ca
Shahyn <shahynkamali@gmail.com>
2019-03-09 14:54:04 -0500

    got another joke

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,30 +1,23 @@
 import React from 'react';
 
 const LandingPage = (props) => {
-    // let topJoke = props.jokesFirebaseUse[0].key;
-    // console.log(topJoke);
     return (
-        // <React.Fragment>
-        //     {()=> topJoke.map((joke) => {
-        //         return(
-        //             <div>
-        //                 <p>test</p>
-        //             </div>
-        //         )
-        //     })}
-        // </React.Fragment>
-        <h1>HI!</h1>
+        <React.Fragment>
+        {
+            props.jokesFirebaseUse.slice(0,1).map((joke=>{
+                return(
+                    <div>
+                        <p>{joke.userJoke}</p>
+                        <p>{joke.userName}</p>
+                    <p>{joke.userDepartment}</p>
+
+                    </div>
+                )
+            }))
+        }
+    </React.Fragment>
     )
     
-//     return (
-
-//     <div className="landing-page">
-//         <h1>THIS IS OUR TOP JOKE</h1>
-//         {/* <p>{props.jokesFirebaseUse[0].userJoke}</p> */}
-//     </div>
-
-
-// )
 }
 
 export default LandingPage;
