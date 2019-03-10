9edccc1e9858f7be803b8217d703f70b39221907
Shahyn <shahynkamali@gmail.com>
2019-03-10 14:03:30 -0400

    created from scss

--- src/components/SubmitJoke.js
+++ src/components/SubmitJoke.js
@@ -4,41 +4,39 @@ const SubmitJoke = (props) => {
 return(
     // Form to submit your joke
     <div className="submit-joke-container">
-
-        {/* Section instructions */}
-        <h2>Submit A Joke!</h2>
-        <p>Some instructions about what to do on the page.</p>
-
-
+        <div className="form-description">
+            <h2>Submit A Joke!</h2>
+            <p>In the form below, please submit your name, department and joke</p>
+        </div>
         {/* FORM STARTS */}
-        <form action="submit" onSubmit={props.handleJokeSubmit}>
-        {/* Name */}
-        <label htmlFor="name" name="name" className="visually-hidden">Please enter your name</label>
-
-        <input  type="text" id="name" name="userName" required placeholder="name"
-        onChange={props.handleChange}
-        value={props.userSubmittedJoke.userName}
-        />
-
-        {/* Department */}
-        <label htmlFor="department" name="department" className="visually-hidden">Please enter your department</label>
-        <input type="text" id="department" name="userDepartment" required placeholder="Your department" 
-        onChange={props.handleChange}
-        value={props.userSubmittedJoke.userDepartment}  
-        />
-
-        {/* Joke */}
-        <label htmlFor="joke" name="joke" className="visually-hidden">Give us your joke!</label>
-        <textarea name="userJoke" id="joke" cols="30" rows="10" required placeholder="Give us your joke!"
-        
-        onChange={props.handleChange}
-        value={props.userSubmittedJoke.userJoke}></textarea>
-
-        <button 
-        type="submit"
-        className="button">
-        Submit it!</button>
-        </form>
+        <div className="form">
+            <form action="submit" onSubmit={props.handleJokeSubmit}>
+                {/* Name */}
+                <div className="input clearfix">
+                    <label htmlFor="name" name="name" className="visually-hidden">Name</label>
+                    <input  type="text" id="name" name="userName" required placeholder="Name"
+                    onChange={props.handleChange}
+                    value={props.userSubmittedJoke.userName}
+                    />
+                    {/* Department */}
+                    <label htmlFor="department" name="department" className="visually-hidden">Department</label>
+                    <input type="text" id="department" name="userDepartment" required placeholder="Department" 
+                    onChange={props.handleChange}
+                    value={props.userSubmittedJoke.userDepartment}  
+                    />
+                </div>
+                <div className="textfield">
+                    {/* Joke */}
+                    <label htmlFor="joke" name="joke" className="visually-hidden">Tell us your joke!</label>
+                    <textarea name="userJoke" id="joke" cols="30" rows="10" required placeholder="Tell us your joke!"
+                    onChange={props.handleChange}
+                    value={props.userSubmittedJoke.userJoke}></textarea>
+                </div>
+                <div className="submit">
+                    <button type="submit" className="button">Submit</button>
+                </div>
+            </form>
+        </div>
     
     </div>
 
