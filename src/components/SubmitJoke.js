da433f333c85c7fd84876dea8ea2d879cdbf9771
Shahyn <shahynkamali@gmail.com>
2019-03-11 18:46:00 -0400

    almost done form

--- src/components/SubmitJoke.js
+++ src/components/SubmitJoke.js
@@ -3,17 +3,15 @@ import React from 'react';
 const SubmitJoke = (props) => {
 return(
     <div className="submit-joke-container">
-        <div className="form-description clearfix">
-            <div className="header">
-                <h2>Submit A Joke!</h2>
-            </div>
-            <div className="desc">
-                <p>In the form below, please submit your name, department and joke</p>
-            </div>
+        <div className="desc">
+            <p>In the form below, please submit your name, department and joke</p>
         </div>
             <form className="clearfix" action="submit" onSubmit={props.handleJokeSubmit}>
                 <div className="form clearfix">
                     <div className="input clearfix">
+                        <div className="header">
+                            <h2>Submit A Joke!</h2>
+                        </div>
                         <div className="form-name clearfix">
                             <label htmlFor="name" name="name" className="visually-hidden">Name</label>
                             <input type="text" id="name" name="userName" required placeholder="Name"
@@ -28,15 +26,15 @@ return(
                             value={props.userSubmittedJoke.userDepartment}  
                             />
                         </div>
-                        <div className="submit">
-                            <button type="submit" className="button">Submit</button>
-                        </div>
                     </div>
                     <div className="textfield">
                         <label htmlFor="joke" name="joke" className="visually-hidden">Tell us your joke!</label>
                         <textarea name="userJoke" id="joke" cols="30" rows="10" required placeholder="Tell us your joke!"
                         onChange={props.handleChange}
                         value={props.userSubmittedJoke.userJoke}></textarea>
+                        <div className="submit">
+                            <button type="submit" className="button">Submit</button>
+                        </div>
                     </div>
                 </div>
             </form>
