fe21af18b0b51ee9f7a86641ba1041427f52b461
Shahyn <shahynkamali@gmail.com>
2019-03-12 15:12:59 -0400

    did stuf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Swal from 'sweetalert2';
 import Header from './components/Header.js'
 import Nav from './components/Nav.js'
